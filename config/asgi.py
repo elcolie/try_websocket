98e6f86af206194f8437042e30725c2dcb7dafb9
Sarit Riwtirune <cs.sarit@gmail.com>
2021-09-25 18:00:29 +0700

    First commit

---
+++ config/asgi.py
@@ -0,0 +1,54 @@
+"""
+ASGI config for try websocket project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/dev/howto/deployment/asgi/
+
+"""
+import os
+import sys
+from pathlib import Path
+
+from channels.auth import AuthMiddlewareStack
+from channels.routing import URLRouter, ProtocolTypeRouter
+from django.core.asgi import get_asgi_application
+
+# This allows easy placement of apps within the interior
+# try_websocket directory.
+import chat.routing
+
+ROOT_DIR = Path(__file__).resolve(strict=True).parent.parent
+sys.path.append(str(ROOT_DIR / "try_websocket"))
+
+# If DJANGO_SETTINGS_MODULE is unset, default to the local settings
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.local")
+
+# This application object is used by any ASGI server configured to use this file.
+django_application = get_asgi_application()
+# Apply ASGI middleware here.
+# from helloworld.asgi import HelloWorldApplication
+# application = HelloWorldApplication(application)
+
+# Import websocket application here, so apps from django_application are loaded first
+from config.websocket import websocket_application  # noqa isort:skip
+
+#
+# async def application(scope, receive, send):
+#     if scope["type"] == "http":
+#         await django_application(scope, receive, send)
+#     elif scope["type"] == "websocket":
+#         await websocket_application(scope, receive, send)
+#     else:
+#         raise NotImplementedError(f"Unknown scope type {scope['type']}")
+import chat.routing
+
+application = ProtocolTypeRouter({
+  "http": get_asgi_application(),
+  "websocket": AuthMiddlewareStack(
+        URLRouter(
+            chat.routing.websocket_urlpatterns
+        )
+    ),
+})
